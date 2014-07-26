bc9a61f3330d811a4e262cc8bf180afcd023a6ea
Coolbit Liu <coolbit.in@gmail.com>
2014-07-26 22:35:39 +0800

    Web view update

---
+++ web/monitor_web.py
@@ -0,0 +1,21 @@
+from bottle import route, run, template, request, static_file
+import os
+@route('/')
+def index():
+    return template('index')
+
+@route('/api')
+def api():
+    rq_type= request.query.type
+    time_b = request.query.b
+    time_unit = request.query.unit
+    rrd_file_name = {'c': 'cpu.rrd', 'm': 'mem.rrd', 'n': 'net.rrd'}[rq_type]
+    img_file_name = {'c': 'cpu.svg', 'm': 'mem.svg', 'n': 'net.svg'}[rq_type]
+    rrd_file_root = '/home/coolbit/computer/python/project_cool_monitor/tmp'
+    img_file_root = 'cache'
+    script_path = '/home/coolbit/computer/python/project_cool_monitor/make_image.py'
+    command_line = 'python {script_path} -{rq_type} -b {time_b}:{time_unit} {rrd_file_root}/{rrd_file_name} {img_file_root}/{img_file_name}'.format(script_path=script_path, rq_type=rq_type, time_b=time_b, time_unit=time_unit, rrd_file_root=rrd_file_root, rrd_file_name=rrd_file_name, img_file_root=img_file_root, img_file_name=img_file_name)
+    os.popen(command_line)
+    return static_file(filename=img_file_name, root=img_file_root, mimetype='image/svg+xml')
+
+run(host='127.0.0.1', port='9001', debug=True)
---
+++ web/views/index.tpl
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta content="text/html" charset="UTF-8">
+  <!--
+  <link rel="stylesheet" href="static/css/bootstrap.css">
+  <script src="static/js/jQuery.js"></script>
+  <script src="static/js/bootstrap.js"></script>
+  -->
+</head>
+<body>
+  <div>
+    <h2>Network</h2>
+    <embed src="/api?type=n&b=1&unit=H" type="image/svg+xml" width="500" height="200"></embed>
+  </div>
+  <div>
+    <h2>Cpu</h2>
+    <embed src="/api?type=c&b=1&unit=H" type="image/svg+xml" width="500" height="200"></embed>
+
+  </div>
+  <div>
+    <h2>Memory</h2>
+    <embed src="/api?type=m&b=1&unit=H" type="image/svg+xml" width="500" height="200"></embed>
+  </div>
+</body>
+</html>
